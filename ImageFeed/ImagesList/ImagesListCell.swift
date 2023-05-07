801ec49a817beb661e93a08fc61ad797995af33e
Dmitry Rodionov <dr@idrp.ru>
2023-05-07 09:10:57 +0300

    error: ImagesListViewController.swift:30: Fatal error: Unexpectedly found nil while implicitly unwrapping an Optional value

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var cellImage: UIImageView!
+
+}
---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Dmitry Rodionov on 29.04.2023.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    @IBOutlet private var tableView: UITableView!
+    
+    private let photosName: [String] = Array(0..<20).map({"\($0)"})
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource  = self
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+        tableView.register(ImagesListCell.self, forCellReuseIdentifier: ImagesListCell.reuseIdentifier)
+    }
+    
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        let photoName = photosName[indexPath.row]
+        guard let image = UIImage(named: photoName) else {
+            print("Failed to load image")
+            return
+        }
+        
+        cell.cellImage.image = image
+    }
+    
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: ImagesListCell.reuseIdentifier
+            , for: indexPath
+        )
+        guard let imageListCell = cell as? ImagesListCell else {
+            print("ImageListCell is nil.")
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell, with: indexPath)
+        
+        return imageListCell
+    }
+    
+    
+}
